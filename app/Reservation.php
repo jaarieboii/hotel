9292636e9a0d2a46a95604f09d251e54eb6d1b6b
Amy Geelhoed <0933602@hr.nl>
2018-05-14 19:35:45 +0200

    Fixed small Reservation errors

--- app/Reservation.php
+++ app/Reservation.php
@@ -10,5 +10,6 @@ class Reservation extends Model
     public function getUserID(){
 
         return User::where('id', $this->user_id)->first()->username;
+        return User::where('name', $this->username)->first()->user_id;
     }
 }
